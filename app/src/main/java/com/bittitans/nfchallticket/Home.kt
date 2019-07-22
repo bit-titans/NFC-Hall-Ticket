638e231c10a17b849faec145b6c4046db1f4881b
Akash <rottiakash@gmail.com>
2019-07-22 12:29:25 +0530

    Added Auth
    Co-authored-by: abhishekbn87 <abhishekbn87@gmail.com>

--- app/src/main/java/com/bittitans/nfchallticket/Home.kt
+++ app/src/main/java/com/bittitans/nfchallticket/Home.kt
@@ -1,11 +1,19 @@
 package com.bittitans.nfchallticket
 
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
+
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,7 +26,10 @@ private const val ARG_PARAM2 = "param2"
  *
  */
 class Home : Fragment() {
+    companion object {
 
+        private const val RC_SIGN_IN = 123
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,5 +38,41 @@ class Home : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().setAllowNewAccounts(false).build())
+
+// Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                Toast.makeText(context,"Hello ${user?.email}",Toast.LENGTH_LONG).show()
+                // ...
+            } else {
+                Toast.makeText(context,"Failed",Toast.LENGTH_LONG).show()
+                System.exit(0)
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
 
 }
