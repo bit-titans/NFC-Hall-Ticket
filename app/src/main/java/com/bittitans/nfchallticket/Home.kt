762bd150ee0fd8ee4a344d412a902f907415b9d4
Akash <rottiakash@gmail.com>
2019-07-22 13:02:53 +0530

    Added Reading Activity
    Co-authored-by: abhishekbn87 <abhishekbn87@gmail.com>

--- app/src/main/java/com/bittitans/nfchallticket/Home.kt
+++ app/src/main/java/com/bittitans/nfchallticket/Home.kt
@@ -9,11 +9,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
-
-
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -40,6 +40,9 @@ class Home : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        scanb.setOnClickListener {
+            findNavController().navigate(R.id.action_home3_to_scanFragment)
+        }
         // Choose authentication providers
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().setAllowNewAccounts(false).build())
